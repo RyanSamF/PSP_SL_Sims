3b2f71aea57f89753637b1fa23e30a975668827f
Morgan Robinson <robin824@purdue.edu>
2025-10-05 14:01:17 -0400

    started altitude optimization code

--- SLUIRP/in_dev/airbrakes_system/altitude_optimization.py
+++ SLUIRP/in_dev/airbrakes_system/altitude_optimization.py
@@ -15,3 +15,21 @@ Get Cd value at starting mach number and starting altitude to reach desired alti
     2) Look at output of function 1 and adjust CD value input based on difference in height 
     3) Repeat 1 and 2 until output altitude = desired altitude (while loop)
 """
+import math
+from midair_sims.py import midair_sim(vehicle, init_vel, init_alt, init_angle)
+from CD_slope_estimation.py import #NAME (cd_file, velocity, cd)
+def cd_equation (init_vel, velocity, cd):
+
+    return cd
+def alt_opt (init_vel, init_angle, init_apogee):
+    area = math.pi*((diameter**2)/4)
+    force_drag = 0.5*(init_vel**2)*1.204*area
+    return
+apogee = 4750
+while actual_apogee != apogee:
+    if actual_apogee < apogee:
+        cd_test = (cd_min + cd)/2
+        actual_apogee = alt_opt(init_vel, init_angle, init_apogee)
+    elif actual_apogee > apogee:
+        cd_test = (cd_max + cd)/2
+        actual_apogee = alt_opt(init_vel, init_angle, init_apogee)
